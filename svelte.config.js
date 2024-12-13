85826631fc1c417f978891847847db41358f2240
Marley Mulvin Broome <marley.developer@gmail.com>
2024-12-13 17:12:41 +0900

    Added vercel adapter with region set to kansai

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,7 @@
 import { escapeSvelte, mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { createHighlighter } from 'shiki';
-import remarkUnwrapImages from 'remark-unwrap-images';
-import remarkToc from 'remark-toc';
-import rehypeSlug from 'rehype-slug';
 
 const theme = 'github-dark';
 
@@ -27,10 +24,9 @@ const config = {
 	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			regions: ["kix1"]
+		})
 	},
 
 	extensions: ['.svelte', '.svx']
